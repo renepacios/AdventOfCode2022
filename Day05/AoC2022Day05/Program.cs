d67b3f033b4526e8f8a8eadb32c144e5d578708d
René Pacios <3912951+renepacios@users.noreply.github.com>
2022-12-08 22:04:16 +0100

    Initial commit

---
+++ Day05/AoC2022Day05/Program.cs
@@ -0,0 +1,34 @@
+﻿using AdventUtils;
+
+
+const bool useTestData = true;
+const bool debug = true;
+
+string dataFilePath = useTestData ? "data/test.txt" : "data/input.txt";
+var fileData = DataReader.Read(dataFilePath, s => s.Trim()).ToList();
+
+
+//part one
+Console.WriteLine("Part One");
+int result = 0;
+foreach (string s in fileData)
+{
+    if (debug) Console.WriteLine($"Line: {s}");
+
+
+}
+
+Console.WriteLine($"Result {result}");
+
+
+//part Two
+Console.WriteLine("Part Two");
+result = 0;
+foreach (string s in fileData)
+{
+    if (debug) Console.WriteLine($"Line: {s}");
+}
+
+Console.WriteLine($"Result {result}");
+
+Console.ReadLine();
