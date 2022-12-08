b38f7a2f5fe756e7e529605584177cf301aa325b
René Pacios <3912951+renepacios@users.noreply.github.com>
2022-12-08 22:09:37 +0100

    reading stacks

--- Day05/AoC2022Day05/Program.cs
+++ Day05/AoC2022Day05/Program.cs
@@ -11,12 +11,19 @@ var fileData = DataReader.Read(dataFilePath, s => s.Trim()).ToList();
 //part one
 Console.WriteLine("Part One");
 int result = 0;
+
+Stack<string> stack = new Stack<string>();
+
 foreach (string s in fileData)
 {
     if (debug) Console.WriteLine($"Line: {s}");
 
+    if(string.IsNullOrEmpty(s)) break;
 
+    stack.Push(s);
 }
+
+
 
 Console.WriteLine($"Result {result}");
 
@@ -24,10 +31,10 @@ Console.WriteLine($"Result {result}");
 //part Two
 Console.WriteLine("Part Two");
 result = 0;
-foreach (string s in fileData)
-{
-    if (debug) Console.WriteLine($"Line: {s}");
-}
+//foreach (string s in fileData)
+//{
+//    if (debug) Console.WriteLine($"Line: {s}");
+//}
 
 Console.WriteLine($"Result {result}");
 
