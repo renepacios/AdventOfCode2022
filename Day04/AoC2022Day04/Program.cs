5c502c282dca9af3fcdeab62929d50e09f1e64dc
René Pacios <3912951+renepacios@users.noreply.github.com>
2022-12-08 21:41:23 +0100

    simple solution to day04

---
+++ Day04/AoC2022Day04/Program.cs
@@ -0,0 +1,45 @@
+﻿using AdventUtils;
+
+
+const bool useTestData = false;
+const bool debug = true;
+
+string dataFilePath = useTestData ? "data/test.txt" : "data/input.txt";
+var fileData = DataReader.Read(dataFilePath, s => s.Trim()).ToList();
+
+
+//part one
+Console.WriteLine("Part One");
+int result = 0;
+int result2=0;
+foreach (string s in fileData)
+{
+    if (debug) Console.WriteLine($"Line: {s}");
+
+    var pares = s.Split(",");
+    var p1 = pares[0].Split("-").Select(s=>Convert.ToInt32(s)).ToArray();
+    var p2 =pares[1].Split("-").Select(s=>Convert.ToInt32(s)).ToArray();
+
+    if ((p1[0] <= p2[0] && p1[1] >= p2[1])
+        || (p1[0] >= p2[0] && p1[1] <= p2[1]))
+    {
+        result++;
+    }
+
+
+    //overlaps
+    if ((p1[0] >= p2[0] && p1[0] <= p2[1]) 
+        || (p1[1] >= p2[0] && p1[1] <= p2[1])
+        || (p2[0] >= p1[0] && p2[0] <= p1[1]) 
+        || (p2[1] >= p1[0] && p2[1] <= p1[1])
+        )
+    {
+        result2++;
+    }
+
+}
+
+Console.WriteLine($"Result {result}");
+Console.WriteLine($"Result2 {result2}");
+
+Console.ReadLine();
