9cb7c070f0f6263b8ad3d93248d7d07c80a84c79
René Pacios <3912951+renepacios@users.noreply.github.com>
2022-12-12 22:17:21 +0100

    dAY07 part two done

--- Day07/AoC2022Day07/Program.cs
+++ Day07/AoC2022Day07/Program.cs
@@ -3,7 +3,7 @@
 using AdventUtils;
 using AoC2022Day07;
 
-const bool useTestData = true;
+const bool useTestData = false;
 
 string dataFilePath = useTestData ? "data/test.txt" : "data/input.txt";
 var fileData = DataReader.Read(dataFilePath, s => s).ToList();
@@ -39,40 +39,33 @@ IEnumerable<ItemFile> GetFoldersWhere(List<ItemFile> fols, Func<ItemFile, bool> 
 
 var folders = GetFoldersWhere(currentFolder.GetRootItem().SubItems, w => w.IsDirectory && w.Size <= 100000).ToList();
 
-foreach (ItemFile itemFile in folders)
-{
-    Console.WriteLine($"Name: {itemFile.Name} Size: {itemFile.Size}");
-}
-Console.Write("\n\n");
+//foreach (ItemFile itemFile in folders)
+//{
+//    Console.WriteLine($"Name: {itemFile.Name} Size: {itemFile.Size}");
+//}
+//Console.Write("\n\n");
 
 Console.WriteLine($"Total Size {folders.Sum(s => s.Size)}");
 
 //part Two
 Console.WriteLine("Part Two");
-var allFiles = GetFoldersWhere(currentFolder.GetRootItem().SubItems, w => w.Type==ItemFile.ItemType.File)
+var allFolders = GetFoldersWhere(currentFolder.GetRootItem().SubItems, w => w.Type==ItemFile.ItemType.Directory)
     //.OrderBy(o=>o.Size)
     .ToList();
 
-long total = allFiles.Sum(s=>s.Size);
+long total = currentFolder.GetRootItem().Size;
 long unUsedSpace = 70000000 - total;
 long necesary = 30000000 - unUsedSpace;
 
 Console.WriteLine($"Total Used: {total} Unused: {unUsedSpace} Necesary: {necesary}");
 
-//foreach (ItemFile itemFile in allFolders.Where(w=>w.Size>= necesary))
+//foreach (ItemFile itemFile in allFolders.Where(w => w.Size >= necesary))
 //{
 //    Console.WriteLine($"\nFolder: {itemFile.Name} - {itemFile.Size}");
 //}
 
-//var item = allFolders.FirstOrDefault(w => w.Size >= necesary);
-//Console.WriteLine($"Folder: {item.Name} - {item.Size}");
+var item = allFolders.FirstOrDefault(w => w.Size >= necesary);
+Console.WriteLine($"\n\nFolder: {item.Name} - {item.Size}");
 
-//foreach (var l in fileData)
-//{
-//    var res1 = PartTwo.DoIt(l);
-//    Console.WriteLine($"Result Two {l} --> {res1}");
-//}
-
-//Console.Write("\n\n");
 
 Console.ReadLine();
