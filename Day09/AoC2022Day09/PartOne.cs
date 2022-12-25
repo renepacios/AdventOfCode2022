da2a93e4efce405e48d8b4cbe3326aa1ec48c4d2
René Pacios <3912951+renepacios@users.noreply.github.com>
2022-12-25 12:23:15 +0100

    llalaal

--- Day09/AoC2022Day09/PartOne.cs
+++ Day09/AoC2022Day09/PartOne.cs
@@ -2,10 +2,44 @@
 {
     internal static class PartOne
     {
-        
+        private static (int x, int y) _currentHeadPosition = (0, 0);
+        private static (int x, int y) _currentTailPosition=(0, 0);
+        private static Dictionary<(int x, int y), int> _hLocations = new Dictionary<(int x, int y), int>();
+        private static Dictionary<(int x, int y), int> _tLocations = new Dictionary<(int x, int y), int>();
+
+        private static void MoveHead(Func<(int x, int y), (int x, int y)> move)
+        {
+
+        }
+
+        private static void GenerateMove(string move)
+        {
+            var items = move.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int m = Convert.ToInt32(items[1]);
+
+
+            Func<(int x, int y), (int, int)> movFunc = items[0] switch
+            {
+                "U" => tuple => (tuple.x, tuple.y - m),
+                "R" => tuple => (tuple.x + m, tuple.y),
+                "D" => tuple => (tuple.x, tuple.y + m),
+                "L" => tuple => (tuple.x - m, tuple.y),
+
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+
+
+        }
+
 
         internal static int DoIt(List<string> data)
         {
+            foreach (string s in data)
+            {
+                GenerateMove(s);
+            }
+
             return -1;
         }
     }
